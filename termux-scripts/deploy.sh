971adb1b617e7d9d6212cbd13bcf56ef6c840eca
245673073-source <245673073@qq.com>
2025-12-19 18:49:28 +0800

    Update deploy.sh

--- termux-scripts/deploy.sh
+++ termux-scripts/deploy.sh
@@ -90,7 +90,8 @@ install_python_packages() {
     #python -m pip install --upgrade pip
     
     # 安装依赖
-    pip install pillow openai requests -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
+    #pip install requests
+    pip install pillow openai -i https://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
     
     print_success "Python 依赖安装完成"
 }
